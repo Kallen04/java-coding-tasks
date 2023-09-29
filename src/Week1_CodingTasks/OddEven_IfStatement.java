a956448c2fb23d86e803c7063addcefdc80dac23
DNAVZM <andjelapopov1@gmail.com>
2023-09-28 20:06:51 -0700

    OddEven_If_Solution

---
+++ src/Week1_CodingTasks/OddEven_IfStatement.java
@@ -0,0 +1,22 @@
+package Week1_CodingTasks;
+
+import java.util.Scanner;
+
+public class OddEven_IfStatement {
+
+    public static void main(String[] args) {
+
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = reader.nextInt();
+
+        if (num % 2 == 0) {
+
+            System.out.println(num + " is even");
+        } else {
+
+            System.out.println(num + " is odd");
+        }
+    }
+}
