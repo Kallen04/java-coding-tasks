26aec7ad047dbfc30f968a1db289de78a3960578
Mahbuba-Nasibova <nma_461@mail.ru>
2023-11-22 12:08:29 -0800

    Task2_Mahbuba updated

--- src/Week6_CodingTasks/Task2_Mahbuba.java
+++ src/Week6_CodingTasks/Task2_Mahbuba.java
@@ -5,7 +5,7 @@ public class Task2_Mahbuba {
     //Write a method that can find the maximum number from an int Array
     public static int findMaxArray(int[] arr) {
         int max = arr[0]; //initialize max,it will hold value of first array
-        for (int i = 0; i < arr.length - 1; i++) { //loop through array
+        for (int i = 0; i < arr.length; i++) { //loop through array
             if (max < arr[i]) { //if any of element is greater than max, then store a value of the element in max
                 max = arr[i]; //at the end of the loop, max represents the largest element in the array
             }
@@ -15,7 +15,7 @@ public class Task2_Mahbuba {
     }
 
     public static void main(String[] args) {
-        int[] arr = {2, 5, -1, 8, 1, 3};
+        int[] arr = {2, 5, -1, 8, 1, 9};
         System.err.println(("Maximum number is: " + findMaxArray(arr)));
     }
 
